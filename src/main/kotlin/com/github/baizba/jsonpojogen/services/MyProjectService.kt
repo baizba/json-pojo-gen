87e7f733419cad956b38d4f089bbf1659840840f
GitHub Action <action@github.com>
2023-08-10 12:43:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/baizba/jsonpojogen/services/MyProjectService.kt
+++ src/main/kotlin/com/github/baizba/jsonpojogen/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.baizba.jsonpojogen.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.baizba.jsonpojogen.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
